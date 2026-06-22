e0ce881b64c085cbb0692779aa4a2599b0214fb3
krby4 <krby4@users.noreply.github.com>
2026-06-22 07:14:26 -0700

    Added auth on add-run page, added add-run page. Stable branch right now

--- app/app.py
+++ app/app.py
@@ -1,8 +1,48 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,g,session,redirect,url_for,abort
 import reports
+from auth import init_auth, oauth, get_username
+import auth
+import runs
 from datetime import datetime
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+init_auth(app)
+app.wsgi_app = ProxyFix(
+    app.wsgi_app,
+    x_proto=1,
+    x_host=1
+)
+# oidc = auth.create_oidc(app)
+
+# @app.before_request
+# def load_identity():
+#     oidc = OpenIDConnect(app)
+#     if oidc.user_loggedin:
+#         g.user = g.oidc_user
+
+#     return
+@app.route("/login")
+def login():
+    return oauth.keycloak.authorize_redirect(
+        url_for("auth_callback", _external=True)
+    )
+@app.route("/auth/callback")
+def auth_callback():
+    token = oauth.keycloak.authorize_access_token()
+    session["user"] = token.get("userinfo")
+    session["token"] = token
+    return redirect(url_for("whoami"))
+@app.route("/whoami")
+def whoami():
+    token = session.get("token", {})
+    return {
+        "user": session.get("user", {}),
+        # "token_keys": list(session.get("token",{}).keys()),
+        # "userinfo_keys": list(token.get("userinfo",{}).keys()),
+        # "token": token,
+    }
+
 @app.route("/")
 
 def hello():
@@ -36,6 +76,29 @@ def user_home(user_id):
         run_type_rows=run_type_rows,
         max_runs_rows=max_runs_rows
     )
+
+@app.route("/user/<user_id>/add-run", methods=["GET","POST"])
+def add_run(user_id):
+    if not auth.can_add_run(user_id):
+        abort(403)
+    if request.method == "GET":
+        return render_template("add_run.html", user_id=user_id)
+    
+    run = runs.validate_manual(
+        date=request.form.get("date"),
+        start=request.form.get("start"),
+        end=request.form.get("end"),
+        duration=request.form.get("duration"),
+        run_type=request.form.get("run_type"),
+        distance=request.form.get("distance"),
+        ave_hr=request.form.get("ave_hr"),
+        max_hr=request.form.get("max_hr"),
+        rpe=request.form.get("rpe"),
+        notes=request.form.get("notes"),
+    )
+
+    runs.insert_run(run,user_id=user_id)
+    return redirect(url_for("user_home",user_id=user_id))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
---
+++ app/auth.py
@@ -0,0 +1,59 @@
+from authlib.integrations.flask_client import OAuth
+from flask import g, session
+from dotenv import load_dotenv
+import os
+
+oauth = OAuth()
+
+def init_auth(app):
+    load_dotenv()
+    app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY","default")
+    oauth.init_app(app)
+    oauth.register(
+        name="keycloak",
+        client_id = os.getenv("KEYCLOAK_CLIENT_ID","default"),
+        client_secret = os.getenv("KEYCLOAK_CLIENT_SECRET","default"),
+        server_metadata_url = os.getenv("KEYCLOAK_METADATA_URL","default"),
+        client_kwargs={
+            "scope" : "openid profile email"
+        },
+    )
+    return oauth
+
+def get_current_user():
+    return session.get("user")
+
+def get_username():
+    user = get_current_user()
+    if not user:
+        return None
+    return user.get("preferred_username")
+
+def get_current_roles():
+    return session.get("roles")
+
+def is_admin():
+    return False
+#     roles = get_current_roles()
+#     if not roles:
+#         return False
+#     elif "running-log-admin" in roles:
+#         return True
+#     else:
+#         return False
+
+def can_add_run(user_id):
+    username = get_username()
+    if not username:
+        return False
+    if is_admin():
+        return True
+    return username == user_id
+
+# def require_user_access(user_id):
+#     if is_admin():
+#         return True
+#     # elif current_user == user_id:
+#     #     return True
+#     else:
+#         return "abort 443"
